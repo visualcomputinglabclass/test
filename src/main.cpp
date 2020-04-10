8a8cc2ec08a8f2106e23d881035d466f8c6a254b
kiwasaki <iwasaki@sys.wakayama-u.ac.jp>
2020-04-10 12:38:59 +0900

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
-
+#include<memory>
 
 int main( int argc, char** argv )
 {
     std::cout << "Hello World\n";
+    std::unique_ptr< int > x = std::make_unique< int >( 3 );
+    std::cout << *x << "\n";
     return 0;
 }
