e80c1b6ade664d7878701cc794cd557b74ecec66
kiwasaki <iwasaki@sys.wakayama-u.ac.jp>
2020-04-10 12:51:10 +0900

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
 #include<memory>
 
-int main( int argc, char** argv )
-{
-    std::cout << "Hello World\n";
-    std::unique_ptr< int > x = std::make_unique< int >( 3 );
-    std::cout << *x << "\n";
-    return 0;
+#define CATCH_CONFIG_MAIN
+#include"catch.hpp"
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
 }
