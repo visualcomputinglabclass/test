75d16e16bed29869d6bd50a1ca9aff0eebfcdaf3
kiwasaki <iwasaki@sys.wakayama-u.ac.jp>
2020-04-10 15:02:25 +0900

    initial commit

--- src/report001/main.cpp
+++ src/report001/main.cpp
@@ -35,6 +35,17 @@ void save_png( const size_t width, const size_t height, const std::vector< float
     stbi_write_png( filename.c_str(), int( width ), int( height ), 3, pixels.data(), 0 );
 }
 
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
 /*
 int main( int argc, char ** argv )
 {
